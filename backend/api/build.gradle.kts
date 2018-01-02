445f9f0a0f6d22e16694fb8e2a75ebd1969eec7d
George Opritescu <ssscripting@gmail.com>
2018-01-02 20:16:26 +0100

    add web and devtools to build.gradle

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("org.jetbrains.kotlin.plugin.spring") version "1.1.51"
 }
+
 apply {
     plugin("org.springframework.boot")
 }
+
 dependencies {
     val springBootVersion: String = parent!!.properties["springBootVersion"] as String
     compile(project(":backend:component"))
-    compile("org.springframework.boot:spring-boot-starter:$springBootVersion")
+    compile("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    compile("org.springframework.boot:spring-boot-devtools:$springBootVersion")
 }
---
+++ backend/api/src/main/kotlin/com/packtpub/HelloController.kt
@@ -0,0 +1,7 @@
+package com.packtpub
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+}
